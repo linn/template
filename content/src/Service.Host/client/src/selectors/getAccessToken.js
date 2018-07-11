b38b48b2bb82a21fc6664fa9771c5b6804f175e5
Adam Campbell <Adam.Campbell@linn.co.uk>
2018-07-11 09:03:02 +0100

    add-files

---
+++ content/src/Service.Host/client/src/selectors/getAccessToken.js
@@ -0,0 +1,19 @@
+﻿export const getAccessToken = state => {
+    if (!state.oidc.user) {
+        return null;
+    }
+
+    return state.oidc.user.access_token;
+};
+
+export const getToggleContext = state => {
+    return state.oidc.user ? state.oidc.user.profile : undefined;
+};
+
+export const getEmail = state => {
+    if (!state.oidc.user || !state.oidc.user.profile) {
+        return null;
+    }
+
+    return state.oidc.user.profile.email;
+};
