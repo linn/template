d6ea6cfb0363b0e8158a9e2f2537dca602aa7037
richard phillips <richard.phillips@linn.co.uk>
2019-07-16 14:55:28 +0100

    some js tidying

--- content/src/Service.Host/client/src/middleware/authorization.js
+++ content/src/Service.Host/client/src/middleware/authorization.js
@@ -1,12 +1,12 @@
-﻿import { CALL_API } from 'redux-api-middleware';
+﻿import { RSAA } from 'redux-api-middleware';
 import { getAccessToken } from '../selectors/getAccessToken';
 
 export default ({ getState }) => next => action => {
-    if (action[CALL_API]) {
-        if (action[CALL_API].options && action[CALL_API].options.requiresAuth) {
-            action[CALL_API].headers = {
-                Authorization: 'Bearer ' + getAccessToken(getState()),
-                ...action[CALL_API].headers
+    if (action[RSAA]) {
+        if (action[RSAA].options && action[RSAA].options.requiresAuth) {
+            action[RSAA].headers = {
+                Authorization: `Bearer ${getAccessToken(getState())}`,
+                ...action[RSAA].headers
             };
         }
     }
