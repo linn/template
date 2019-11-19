10885c4556e69634607da5d23967b12ead49dd3e
lewisre <lewisre@PC1152>
2019-11-19 09:39:20 +0000

    use dev webpack config

--- content/src/Service.Host/webpack/server.js
+++ content/src/Service.Host/webpack/server.js
@@ -2,7 +2,7 @@
 
 const WebpackDevServer = require('webpack-dev-server');
 
-const config = require('./webpack.prod.config');
+const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
