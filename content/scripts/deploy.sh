486f06c4ff09162c450e8ad93c72a5b73920a491
Richard Phillips <richard.phillips@linn.co.uk>
2021-11-12 10:54:56 +0000

    update scripts

--- content/scripts/deploy.sh
+++ content/scripts/deploy.sh
@@ -2,13 +2,12 @@
 set -ev
 
 # install aws cli
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install -b ~/bin/aws
-export PATH=~/bin:$PATH
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 # deploy on aws
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${TRAVIS_BRANCH}" = "main" ]; then
   if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     # master - deploy to production
     echo deploy to production
@@ -31,21 +30,14 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
     ENV_SUFFIX=-sys
   fi
 else
-  # not master - deploy to int
-  echo deploy to int
-
-    aws s3 cp s3://$S3_BUCKET_NAME/template/int.env ./secrets.env
-
-    STACK_NAME=template-int
-    APP_ROOT=http://app-int.linn.co.uk
-    PROXY_ROOT=http://app.linn.co.uk
-    ENV_SUFFIX=-int
+  # not main - deploy to int if required
+  echo do not deploy to int
 fi
 
 # load the secret variables but hide the output from the travis log
 source ./secrets.env > /dev/null 2>&1
 
 # deploy the service to amazon
-aws cloudformation deploy --stack-name $STACK_NAME --template-file ./aws/application.yml --parameter-overrides dockerTag=$TRAVIS_BUILD_NUMBER databaseHost=$DATABASE_HOST databaseName=$DATABASE_NAME databaseUserId=$DATABASE_USER_ID databasePassword=$DATABASE_PASSWORD rabbitServer=$RABBIT_SERVER rabbitPort=$RABBIT_PORT rabbitUsername=$RABBIT_USERNAME rabbitPassword=$RABBIT_PASSWORD appRoot=$APP_ROOT proxyRoot=$PROXY_ROOT authorityUri=$AUTHORITY_URI loggingEnvironment=$LOG_ENVIRONMENT loggingMaxInnerExceptionDepth=$LOG_MAX_INNER_EXCEPTION_DEPTH environmentSuffix=$ENV_SUFFIX --capabilities=CAPABILITY_IAM
+aws cloudformation deploy --stack-name $STACK_NAME --template-file ./aws/application.yml --parameter-overrides dockerTag=$TRAVIS_BUILD_NUMBER databaseHost=$DATABASE_HOST databaseName=$DATABASE_NAME databaseUserId=$DATABASE_USER_ID databasePassword=$DATABASE_PASSWORD rabbitServer=$RABBIT_SERVER rabbitPort=$RABBIT_PORT rabbitUsername=$RABBIT_USERNAME rabbitPassword=$RABBIT_PASSWORD appRoot=$APP_ROOT proxyRoot=$PROXY_ROOT authorityUri=$AUTHORITY_URI loggingEnvironment=$LOG_ENVIRONMENT loggingMaxInnerExceptionDepth=$LOG_MAX_INNER_EXCEPTION_DEPTH smtpHostname=$SMTP_HOSTNAME shipfilesTestAddress=$SHIPFILES_TEST_ADDRESS shipfilesFromAddress=$SHIPFILES_FROM_ADDRESS pdfServiceRoot=$PDF_SERVICE_ROOT shipfileTemplatePath=$SHIPFILE_TEMPLATE_PATH environmentSuffix=$ENV_SUFFIX --capabilities=CAPABILITY_IAM
 
 echo "deploy complete"
