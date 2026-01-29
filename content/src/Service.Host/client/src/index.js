6d8aa88f8e5b2cf5d75b26a5174de062b46995bf
Richard Phillips <richard.phillips@linn.co.uk>
2026-01-29 15:37:14 +0000

    client, tests, etc

--- content/src/Service.Host/client/src/index.js
+++ content/src/Service.Host/client/src/index.js
@@ -4,37 +4,15 @@ import { SnackbarProvider } from 'notistack';
 import { ThemeProvider, StyledEngineProvider, createTheme } from '@mui/material/styles';
 import { AuthProvider } from 'react-oidc-context';
 import CssBaseline from '@mui/material/CssBaseline';
-import { WebStorageStateStore } from 'oidc-client-ts';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import Root from './components/Root';
 import 'typeface-roboto';
-import config from './config';
+import { oidcConfig } from './helpers/authUtils';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-
-const host = window.location.origin;
-
-const oidcConfig = {
-    authority: config.authorityUri,
-    client_id: 'app2',
-    response_type: 'code',
-    scope: 'openid profile email associations',
-    redirect_uri: `${host}/template/`,
-    post_logout_redirect_uri: `${config.proxyRoot}/authentication/Account/Logout`,
-    onSigninCallback: () => {
-        const redirect = sessionStorage.getItem('auth:redirect');
-        if (redirect) {
-            window.location.href = redirect;
-            sessionStorage.removeItem('auth:redirect');
-        } else {
-            window.location.href = `${host}/template`;
-        }
-    },
-    userStore: new WebStorageStateStore({ store: window.localStorage })
-};
 
 const theme = createTheme({});
 const render = Component => {
